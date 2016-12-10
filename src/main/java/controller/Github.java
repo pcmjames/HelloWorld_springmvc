d722513fd02b7167b4c8652086053182eb420152
PCM-PC\PCM <pcmjames@gmail.com>
2016-12-10 15:00:09 +0800

    no message

---
+++ src/main/java/controller/Github.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * Created by PCM on 2016-12-10.
+ */
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("new class files");
+    }
+}
