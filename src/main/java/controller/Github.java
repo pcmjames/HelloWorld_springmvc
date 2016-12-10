ebc90b87ee3401a700aa8bcd96efc231dc4b7767
PCM-PC\PCM <pcmjames@gmail.com>
2016-12-10 15:34:21 +0800

    no message

--- src/main/java/controller/Github.java
+++ src/main/java/controller/Github.java
@@ -7,4 +7,8 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("new class files");
     }
+
+    public void hello(String name){
+        System.out.println(name);
+    }
 }
